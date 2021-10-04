5b856535a34bc2be25052dc9a2badc699d0e88b7
Alex <ashroyer@gmail.com>
2021-10-04 10:38:35 -0400

    updates

---
+++ docs/foobar.c
@@ -0,0 +1,3 @@
+void foo(int *x){x[0] += 3;}
+int bar(int *a){return a[1];}
+int baz(int *a, int b){return a[b];}
---
+++ docs/mylib.c
@@ -0,0 +1,6 @@
+/* mylib.c */
+int foo(int x, char y){
+    if(y=='a')
+        return x+1000;
+    return 10-x;
+}
